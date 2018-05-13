8935b8fe62cfb1209ce6722e26fc12dff8173634
Mendy <mendybeltcohen@gmail.com>
2018-05-13 17:53:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function createRock(x) {
   // We should kick of the animation of the rock around here
    if(START === true){
   //window.requestAnimationFrame(createRock);
-  window.requestAnimationFrame(createRock);
+  window.requestAnimationFrame(ROCKS);
    }
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
@@ -117,7 +117,7 @@ function createRock(x) {
   // Finally, return the rock element you've created
   return rock;
 }
-   //window.requestAnimationFrame(createRock);
+   //window.requestAnimationFrame(ROCKS);
 /**
  * End the game by clearing `gameInterval`,
  * removing all ROCKS from the DOM,
