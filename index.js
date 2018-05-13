a4bb08b3d981af8036431d9523f89da758335671
Mendy <mendybeltcohen@gmail.com>
2018-05-13 17:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -107,8 +107,8 @@ function createRock(x) {
 
   // We should kick of the animation of the rock around here
    if(START === true){
-  //window.requestAnimationFrame(createRock);
-  window.requestAnimationFrame('.rock');
+  window.requestAnimationFrame(createRock);
+  //window.requestAnimationFrame(moveRock);
    }
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
